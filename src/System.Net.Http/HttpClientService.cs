a24d92d2b8c0e7bf5cf9a22508d1e26a8d455637
JeremyTCD <jeremytanchongdao@gmail.com>
2018-08-03 22:10:19 +0800

    Initial commit

---
+++ src/System.Net.Http/HttpClientService.cs
@@ -0,0 +1,199 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Net.Http.Headers;
+
+namespace Jering.IocServices.System.Net.Http
+{
+    public class HttpClientService : IHttpClientService
+    {
+        private HttpClient _httpClient;
+
+        public HttpRequestHeaders DefaultRequestHeaders { get => _httpClient.DefaultRequestHeaders; }
+        public Uri BaseAddress { get => _httpClient.BaseAddress; set => _httpClient.BaseAddress = value; }
+        public TimeSpan Timeout { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public long MaxResponseContentBufferSize { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public HttpClientService()
+        {
+            _httpClient = (new HttpClient());
+        }
+
+        public void CancelPendingRequests()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> DeleteAsync(string requestUri)
+        {
+            return _httpClient.DeleteAsync(requestUri);
+        }
+
+        public Task<HttpResponseMessage> DeleteAsync(Uri requestUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> DeleteAsync(string requestUri, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> DeleteAsync(Uri requestUri, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> GetAsync(string requestUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> GetAsync(Uri requestUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> GetAsync(string requestUri, HttpCompletionOption completionOption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> GetAsync(string requestUri, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> GetAsync(Uri requestUri, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> GetAsync(string requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> GetByteArrayAsync(string requestUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<byte[]> GetByteArrayAsync(Uri requestUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stream> GetStreamAsync(string requestUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stream> GetStreamAsync(Uri requestUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetStringAsync(string requestUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetStringAsync(Uri requestUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PatchAsync(string requestUri, HttpContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PatchAsync(Uri requestUri, HttpContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PatchAsync(string requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PatchAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PutAsync(string requestUri, HttpContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PutAsync(string requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/System.Net.Http/IHttpClientService.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Net.Http.Headers;
+
+namespace Jering.IocServices.System.Net.Http
+{
+    public interface IHttpClientService
+    {
+        HttpRequestHeaders DefaultRequestHeaders { get; set; }
+        Uri BaseAddress { get; set; }
+        TimeSpan Timeout { get; set; }
+        Int64 MaxResponseContentBufferSize { get; set; }
+
+        Task<String> GetStringAsync(String requestUri);
+        Task<String> GetStringAsync(Uri requestUri);
+        Task<Byte[]> GetByteArrayAsync(String requestUri);
+        Task<Byte[]> GetByteArrayAsync(Uri requestUri);
+        Task<Stream> GetStreamAsync(String requestUri);
+        Task<Stream> GetStreamAsync(Uri requestUri);
+        Task<HttpResponseMessage> GetAsync(String requestUri);
+        Task<HttpResponseMessage> GetAsync(Uri requestUri);
+        Task<HttpResponseMessage> GetAsync(String requestUri, HttpCompletionOption completionOption);
+        Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption);
+        Task<HttpResponseMessage> GetAsync(String requestUri, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> GetAsync(Uri requestUri, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> GetAsync(String requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> GetAsync(Uri requestUri, HttpCompletionOption completionOption, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PostAsync(String requestUri, HttpContent content);
+        Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content);
+        Task<HttpResponseMessage> PostAsync(String requestUri, HttpContent content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PostAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PutAsync(String requestUri, HttpContent content);
+        Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content);
+        Task<HttpResponseMessage> PutAsync(String requestUri, HttpContent content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PutAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PatchAsync(String requestUri, HttpContent content);
+        Task<HttpResponseMessage> PatchAsync(Uri requestUri, HttpContent content);
+        Task<HttpResponseMessage> PatchAsync(String requestUri, HttpContent content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> PatchAsync(Uri requestUri, HttpContent content, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> DeleteAsync(String requestUri);
+        Task<HttpResponseMessage> DeleteAsync(Uri requestUri);
+        Task<HttpResponseMessage> DeleteAsync(String requestUri, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> DeleteAsync(Uri requestUri, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request);
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption);
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, HttpCompletionOption completionOption, CancellationToken cancellationToken);
+        void CancelPendingRequests();
+    }
+}
