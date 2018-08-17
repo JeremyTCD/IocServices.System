608207fc7c222d965bd0149b0b19e7ae475cb338
JeremyTCD <jeremytanchongdao@gmail.com>
2018-08-17 15:58:22 +0800

    Renamed and moved System.IO project

--- src/System.IO/FileService.cs
+++ src/System.IO/FileService.cs
@@ -7,232 +7,232 @@ namespace Jering.IocServices.System.IO
 {
     public class FileService : IFileService
     {
-        public StreamReader OpenText(String path)
+        public StreamReader OpenText(string path)
         {
             return File.OpenText(path);
         }
 
-        public StreamWriter CreateText(String path)
+        public StreamWriter CreateText(string path)
         {
             return File.CreateText(path);
         }
 
-        public StreamWriter AppendText(String path)
+        public StreamWriter AppendText(string path)
         {
             return File.AppendText(path);
         }
 
-        public void Copy(String sourceFileName, String destFileName)
+        public void Copy(string sourceFileName, string destFileName)
         {
             File.Copy(sourceFileName, destFileName);
         }
 
-        public void Copy(String sourceFileName, String destFileName, Boolean overwrite)
+        public void Copy(string sourceFileName, string destFileName, bool overwrite)
         {
             File.Copy(sourceFileName, destFileName, overwrite);
         }
 
-        public FileStream Create(String path)
+        public FileStream Create(string path)
         {
             return File.Create(path);
         }
 
-        public FileStream Create(String path, Int32 bufferSize)
+        public FileStream Create(string path, int bufferSize)
         {
             return File.Create(path, bufferSize);
         }
 
-        public FileStream Create(String path, Int32 bufferSize, FileOptions options)
+        public FileStream Create(string path, int bufferSize, FileOptions options)
         {
             return File.Create(path, bufferSize, options);
         }
 
-        public void Delete(String path)
+        public void Delete(string path)
         {
             File.Delete(path);
         }
 
-        public Boolean Exists(String path)
+        public bool Exists(string path)
         {
             return File.Exists(path);
         }
 
-        public FileStream Open(String path, FileMode mode)
+        public FileStream Open(string path, FileMode mode)
         {
             return File.Open(path, mode);
         }
 
-        public FileStream Open(String path, FileMode mode, FileAccess access)
+        public FileStream Open(string path, FileMode mode, FileAccess access)
         {
             return File.Open(path, mode, access);
         }
 
-        public FileStream Open(String path, FileMode mode, FileAccess access, FileShare share)
+        public FileStream Open(string path, FileMode mode, FileAccess access, FileShare share)
         {
             return File.Open(path, mode, access, share);
         }
 
-        public void SetCreationTime(String path, DateTime creationTimeUtc)
+        public void SetCreationTime(string path, DateTime creationTimeUtc)
         {
             File.SetCreationTime(path, creationTimeUtc);
         }
 
-        public void SetCreationTimeUtc(String path, DateTime creationTime)
+        public void SetCreationTimeUtc(string path, DateTime creationTime)
         {
             File.SetCreationTimeUtc(path, creationTime);
         }
 
-        public DateTime GetCreationTime(String path)
+        public DateTime GetCreationTime(string path)
         {
             return File.GetCreationTime(path);
         }
 
-        public DateTime GetCreationTimeUtc(String path)
+        public DateTime GetCreationTimeUtc(string path)
         {
             return File.GetCreationTimeUtc(path);
         }
 
-        public void SetLastAccessTime(String path, DateTime lastAccessTime)
+        public void SetLastAccessTime(string path, DateTime lastAccessTime)
         {
             File.SetLastAccessTime(path, lastAccessTime);
         }
 
-        public void SetLastAccessTimeUtc(String path, DateTime lastAccessTimeUtc)
+        public void SetLastAccessTimeUtc(string path, DateTime lastAccessTimeUtc)
         {
             File.SetLastAccessTimeUtc(path, lastAccessTimeUtc);
         }
 
-        public DateTime GetLastAccessTime(String path)
+        public DateTime GetLastAccessTime(string path)
         {
             return File.GetLastAccessTime(path);
         }
 
-        public DateTime GetLastAccessTimeUtc(String path)
+        public DateTime GetLastAccessTimeUtc(string path)
         {
             return File.GetLastAccessTimeUtc(path);
         }
 
-        public void SetLastWriteTime(String path, DateTime lastWriteTime)
+        public void SetLastWriteTime(string path, DateTime lastWriteTime)
         {
             File.SetLastWriteTime(path, lastWriteTime);
         }
 
-        public void SetLastWriteTimeUtc(String path, DateTime lastWriteTimeUtc)
+        public void SetLastWriteTimeUtc(string path, DateTime lastWriteTimeUtc)
         {
             File.SetLastWriteTimeUtc(path, lastWriteTimeUtc);
         }
 
-        public DateTime GetLastWriteTime(String path)
+        public DateTime GetLastWriteTime(string path)
         {
             return File.GetLastWriteTime(path);
         }
 
-        public DateTime GetLastWriteTimeUtc(String path)
+        public DateTime GetLastWriteTimeUtc(string path)
         {
             return File.GetLastWriteTimeUtc(path);
         }
 
-        public FileAttributes GetAttributes(String path)
+        public FileAttributes GetAttributes(string path)
         {
             return File.GetAttributes(path);
         }
 
-        public void SetAttributes(String path, FileAttributes fileAttributes)
+        public void SetAttributes(string path, FileAttributes fileAttributes)
         {
             File.SetAttributes(path, fileAttributes);
         }
 
-        public FileStream OpenRead(String path)
+        public FileStream OpenRead(string path)
         {
             return File.OpenRead(path);
         }
 
-        public FileStream OpenWrite(String path)
+        public FileStream OpenWrite(string path)
         {
             return File.OpenWrite(path);
         }
 
-        public String ReadAllText(String path)
+        public string ReadAllText(string path)
         {
             return File.ReadAllText(path);
         }
 
-        public String ReadAllText(String path, Encoding encoding)
+        public string ReadAllText(string path, Encoding encoding)
         {
             return File.ReadAllText(path, encoding);
         }
 
-        public void WriteAllText(String path, String contents)
+        public void WriteAllText(string path, string contents)
         {
             File.WriteAllText(path, contents);
         }
 
-        public void WriteAllText(String path, String contents, Encoding encoding)
+        public void WriteAllText(string path, string contents, Encoding encoding)
         {
             File.WriteAllText(path, contents, encoding);
         }
 
-        public Byte[] ReadAllBytes(String path)
+        public byte[] ReadAllBytes(string path)
         {
             return File.ReadAllBytes(path);
         }
 
-        public void WriteAllBytes(String path, Byte[] bytes)
+        public void WriteAllBytes(string path, byte[] bytes)
         {
             File.WriteAllBytes(path, bytes);
         }
 
-        public String[] ReadAllLines(String path)
+        public string[] ReadAllLines(string path)
         {
             return File.ReadAllLines(path);
         }
 
-        public String[] ReadAllLines(String path, Encoding encoding)
+        public string[] ReadAllLines(string path, Encoding encoding)
         {
             return File.ReadAllLines(path, encoding);
         }
 
-        public IEnumerable<String> ReadLines(String path)
+        public IEnumerable<string> ReadLines(string path)
         {
             return File.ReadLines(path);
         }
 
-        public IEnumerable<String> ReadLines(String path, Encoding encoding)
+        public IEnumerable<string> ReadLines(string path, Encoding encoding)
         {
             return File.ReadLines(path, encoding);
         }
 
-        public void WriteAllLines(String path, IEnumerable<String> contents)
+        public void WriteAllLines(string path, IEnumerable<string> contents)
         {
             File.WriteAllLines(path, contents);
         }
 
-        public void WriteAllLines(String path, IEnumerable<String> contents, Encoding encoding)
+        public void WriteAllLines(string path, IEnumerable<string> contents, Encoding encoding)
         {
             File.WriteAllLines(path, contents, encoding);
         }
 
-        public void AppendAllText(String path, String contents)
+        public void AppendAllText(string path, string contents)
         {
             File.AppendAllText(path, contents);
         }
 
-        public void AppendAllText(String path, String contents, Encoding encoding)
+        public void AppendAllText(string path, string contents, Encoding encoding)
         {
             File.AppendAllText(path, contents, encoding);
         }
 
-        public void AppendAllLines(String path, IEnumerable<String> contents)
+        public void AppendAllLines(string path, IEnumerable<string> contents)
         {
             File.AppendAllLines(path, contents);
         }
 
-        public void AppendAllLines(String path, IEnumerable<String> contents, Encoding encoding)
+        public void AppendAllLines(string path, IEnumerable<string> contents, Encoding encoding)
         {
             File.AppendAllLines(path, contents, encoding);
         }
 
-        public void Move(String sourceFileName, String destFileName)
+        public void Move(string sourceFileName, string destFileName)
         {
             File.Move(sourceFileName, destFileName);
         }
---
+++ src/System.IO/IFileService.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Jering.IocServices.System.IO
+{
+    public interface IFileService
+    {
+        StreamReader OpenText(string path);
+        StreamWriter CreateText(string path);
+        StreamWriter AppendText(string path);
+        void Copy(string sourceFileName, string destFileName);
+        void Copy(string sourceFileName, string destFileName, bool overwrite);
+        FileStream Create(string path);
+        FileStream Create(string path, int bufferSize);
+        FileStream Create(string path, int bufferSize, FileOptions options);
+        void Delete(string path);
+        bool Exists(string path);
+        FileStream Open(string path, FileMode mode);
+        FileStream Open(string path, FileMode mode, FileAccess access);
+        FileStream Open(string path, FileMode mode, FileAccess access, FileShare share);
+        void SetCreationTime(string path, DateTime creationTimeUtc);
+        void SetCreationTimeUtc(string path, DateTime creationTime);
+        DateTime GetCreationTime(string path);
+        DateTime GetCreationTimeUtc(string path);
+        void SetLastAccessTime(string path, DateTime lastAccessTime);
+        void SetLastAccessTimeUtc(string path, DateTime lastAccessTimeUtc);
+        DateTime GetLastAccessTime(string path);
+        DateTime GetLastAccessTimeUtc(string path);
+        void SetLastWriteTime(string path, DateTime lastWriteTime);
+        void SetLastWriteTimeUtc(string path, DateTime lastWriteTimeUtc);
+        DateTime GetLastWriteTime(string path);
+        DateTime GetLastWriteTimeUtc(string path);
+        FileAttributes GetAttributes(string path);
+        void SetAttributes(string path, FileAttributes fileAttributes);
+        FileStream OpenRead(string path);
+        FileStream OpenWrite(string path);
+        string ReadAllText(string path);
+        string ReadAllText(string path, Encoding encoding);
+        void WriteAllText(string path, string contents);
+        void WriteAllText(string path, string contents, Encoding encoding);
+        byte[] ReadAllBytes(string path);
+        void WriteAllBytes(string path, byte[] bytes);
+        string[] ReadAllLines(string path);
+        string[] ReadAllLines(string path, Encoding encoding);
+        IEnumerable<string> ReadLines(string path);
+        IEnumerable<string> ReadLines(string path, Encoding encoding);
+        void WriteAllLines(string path, IEnumerable<string> contents);
+        void WriteAllLines(string path, IEnumerable<string> contents, Encoding encoding);
+        void AppendAllText(string path, string contents);
+        void AppendAllText(string path, string contents, Encoding encoding);
+        void AppendAllLines(string path, IEnumerable<string> contents);
+        void AppendAllLines(string path, IEnumerable<string> contents, Encoding encoding);
+        void Move(string sourceFileName, string destFileName);
+    }
+}
